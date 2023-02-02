eee30ba1ec0494e00e26fe6a72d8586a10ed8c0e
Alex_web <30694838+AlexWeb-ux@users.noreply.github.com>
2023-02-02 23:47:14 +0200

    02/02

--- js/common.js
+++ js/common.js
@@ -198,12 +198,16 @@ document.addEventListener('DOMContentLoaded', function() {
 	})
 	//top
 	var btn = $('#button');
-   $(window).scroll(function() {
-     if ($(window).scrollTop() > 300) {
-       btn.addClass('show');
-     } else {
-       btn.removeClass('show');
-     }
-   }); 
+	$(window).scroll(function() {
+		if ($(window).scrollTop() > 300) {
+			btn.addClass('show');
+		} else {
+			btn.removeClass('show');
+		}
+	});
+	btn.on('click', function(e) {
+		e.preventDefault();
+		$('html, body').animate({scrollTop:0}, '300');
+	});
 	
 });
