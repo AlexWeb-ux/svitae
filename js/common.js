42609e31598ca323ebd1279e0d549ee7d2c88eb9
Alex_web <30694838+AlexWeb-ux@users.noreply.github.com>
2023-02-03 10:46:41 +0200

    02.03

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
 var $page = $('html, body');
 $('a[href*="#"]').click(function() {
     $page.animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
+      scrollTop: $($.attr(this, 'href')).offset().top
     }, 500);
     return false;
 });
