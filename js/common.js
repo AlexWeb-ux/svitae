20cc25ff38fd251333b3e9039c4b16101a683a91
Alex_web <30694838+AlexWeb-ux@users.noreply.github.com>
2023-02-03 08:26:43 +0200

    03/02

--- js/common.js
+++ js/common.js
@@ -183,7 +183,7 @@ $('a[href*="#"]').click(function() {
     this.classList.toggle('opened');
 		$('.mobile__nav').toggle('hidden');
   });
-	var navmobile = document.querySelector('.mobile__nav a');
+	var navmobile = document.querySelector('.mobile__link');
 	navmobile .addEventListener('click', function(a) {
     $('.mobile__nav').addClass('hidden');
 		$('.nav-toggle').removeClass('opened');
